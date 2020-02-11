d71fe46d641c38aa4c1f42e8056659e078308753
FoxDelioncourt <fox.delioncourt@gmail.com>
2020-02-11 15:17:52 +0400

    added basic routes

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,13 @@ app.use(cors());
 
 app.get("/status", (req, res) => {
   res.send({
-    message: "hello world!"
+    message: "Hello!"
+  });
+});
+
+app.post("/register", (req, res) => {
+  res.send({
+    message: `${req.body.email}, registered`
   });
 });
 
